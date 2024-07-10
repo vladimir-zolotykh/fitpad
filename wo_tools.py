5e159826d5188f9c3a0fd7e0677599d7f18d6870
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-07-10 18:33:32 +0300

    Initial import
    Segregated functions grid_info(), rows_info()

---
+++ wo_tools.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import tkinter as tk
+from typing import Dict, List
+from prettytable import PrettyTable
+
+
+def grid_info(container: tk.Frame) -> str:
+    num_cols, num_rows = container.grid_size()
+    table = PrettyTable()
+    # table.align = 'l'
+    table.field_names = ['row', 'num columns']
+    for num_row in range(num_rows):
+        table.add_row([num_row, len(container.grid_slaves(row=num_row))])
+    return str(table)
+
+
+def rows_info(rows: Dict[int, List[tk.Widget]]) -> str:
+    table = PrettyTable()
+    table.field_names = ['row', 'num columns']
+    for row in range(len(rows), 1):
+        table.add_row([row, len(rows[row])])
+    return str(table)
