56d9cd01d99d8dbc1a360a551d53c5e0dd874bf2
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-07-24 17:59:28 +0300

    Add get_exer() local function

--- make_workout.py
+++ make_workout.py
@@ -5,19 +5,27 @@ from datetime import datetime
 from models import Exercise, Workout
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
-
+# $ rm fitpad.db
+# $ python make_exercise_table.py
+# $ python read_exercise_tables.py
+# $ python python make_workout.py
+# $ $ sqlite3 fitpad.db
+# SQLite version 3.34.1 2021-01-20 14:10:07
+# Enter ".help" for usage hints.
+# sqlite> select * from workout;
+# 1|2024-07-24 17:56:43|5|50.0
+# 2|2024-07-24 17:56:43|5|60.0
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///fitpad.db', echo=True)
-    now = datetime.now()
-    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     with Session(engine) as session:
-        stmt = select(Exercise).where(Exercise.name == 'front squat')
-        front_squat = session.scalar(stmt)
-        stmt = select(Exercise).where(Exercise.name == 'squat')
-        squat = session.scalar(stmt)
-        workout1 = Workout(exercise=front_squat, when=now, reps=5, weight=50.0)
-        session.add(workout1)
-        workout2 = Workout(exercise=squat, when=now, reps=5, weight=60.0)
-        session.add(workout2)
+        def get_exer(name: str) -> Exercise:
+            return session.scalar(
+                select(Exercise).where(Exercise.name == name))
+
+        session.add_all([Workout(exercise=get_exer('front squat'),
+                                 when=now, reps=5, weight=50.0),
+                         Workout(exercise=get_exer('squat'),
+                                 when=now, reps=5, weight=60.0)])
         session.commit()
