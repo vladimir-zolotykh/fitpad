3a3c20a415ba38a7a8ebead8154b59b7af75bbd7
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-07-27 17:47:57 +0300

    Add `Save workout` menu (placeholder)

--- start_wo.py
+++ start_wo.py
@@ -38,6 +38,10 @@ class Workout(tk.Tk):
         for name in self.exer_dir:
             _add_exer = partial(dir.add_exer, name)
             add_exer_menu.add_command(label=name, command=_add_exer)
+        menubar.add_command(label='Save workout', command=self.save_workout)
+
+    def save_workout(self):
+        pass
 
 
 parser = argparse.ArgumentParser(
