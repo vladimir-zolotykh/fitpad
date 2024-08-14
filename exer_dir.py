578a7cfa472443d7a521305e502f659677d41475
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-08-14 10:03:18 +0300

    Initial import
    Moved from exertk.py

---
+++ exer_dir.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from typing import Dict
+import tkinter as tk
+from exertk import ExerTk
+from frame2d import Frame2D
+from numbered_frame import NumberedFrame
+from wo_tools import Wo
+
+
+class ExerDir(Dict[str, ExerTk]):
+    """A dict of Workout exercises
+
+    Dict[str, ExerTk]
+    """
+
+    def __init__(self, frame: Frame2D):
+        super().__init__()
+        self.frame: Frame2D = frame
+        self.row: int = 0
+
+    def del_exer(self, name: str):
+        del self[name]
+        self.row -= 1
+
+    def edit_exer(self):
+        wo = Wo(self.frame)
+        for exer in wo:
+            if 0 < exer.exer_no():
+                exer.grid_forget()
+        row: int = 0
+        for exer in wo:
+            if exer.exer_no() == 0:
+                self.del_exer(exer.exer_name())
+                exer.destroy()
+            else:
+                exer.grid(column=0, row=row, sticky=tk.EW)
+                row += 1
+
+    def add_exer(self, name: str):
+        numbered_frame = NumberedFrame(self.frame, self.row)
+        self[name] = ExerTk(numbered_frame.exer_box, name, 0)
+        self.row += 1
