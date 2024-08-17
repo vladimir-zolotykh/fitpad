a2b81ce3457fddab01380f9cd10f33c67fdb44b1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-08-17 17:48:13 +0300

    Add placeholder Frame2D.sort()

--- frame2d.py
+++ frame2d.py
@@ -34,6 +34,20 @@ class Frame2D(tk.Frame):
             raise IndexError(f'Invalid index {rowcol}. '
                              f'Expected ({row_max = }, {col_max = })')
 
+    def sort(self, key=None, from_=None, to=None):
+        """Sort rows from `from_' to `to' inclusive
+
+        Rows are .grid_forget (-ed), sorted, then .grid (-ed) again in
+        the new order. Before forgetting the widget, save its options
+        (how it was packed), re -pack the widget how it was orignally
+        packed (apply the saved options)
+
+        The default `key' function assumes that the 1st column of a
+        row has the EntryVar widget. The function returns the
+        EntryVar's associated variable value converted to an integer
+        """
+        pass
+
 
 if __name__ == '__main__':
     root = tk.Tk()
