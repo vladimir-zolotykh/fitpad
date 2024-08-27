e82cb418b225b605c377ae492cc6c5505718e006
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-08-27 10:53:45 +0300

    Fix Frame2DSet.arrange()
    +                    if col == 0:
    +                        var: tk.StringVar = e.configure('textvariable')
    +                        var.set(str(new_set_no))
    +                        new_set_no += 1
    Only 1st EntryVar widget is a set number holder.

--- frame2d_set.py
+++ frame2d_set.py
@@ -33,19 +33,20 @@ class Frame2DSet(f2.Frame2D):
             set_no: EntryVar = row[0][0]
             saved_entry: SavedEntry
             e: EntryVar
+            new_set_no: int = 1
             if int(set_no.get()) == 0:
                 for saved_entry in row:
                     e = saved_entry[0]
                     e.destroy()
             else:
-                new_set_no: int = 1
-                for saved_entry in row:
+                for col, saved_entry in enumerate(row):
                     e = saved_entry[0]
-                    var: tk.StringVar = e.configure('textvariable')
-                    var.set(str(new_set_no))
-                    new_set_no += 1
+                    if col == 0:
+                        var: tk.StringVar = e.configure('textvariable')
+                        var.set(str(new_set_no))
+                        new_set_no += 1
                     o: Info = saved_entry[1]
                     e.grid(column=o['column'], row=row_index,
                            sticky=o['sticky'])
-                    row_index += 1
+                row_index += 1
         mb.grid(column=0, row=row_index + 1, columnspan=cols)
