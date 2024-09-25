20608c57d3c7f9acf9c27b162438b6edd6a76a8f
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-09-25 20:23:53 +0300

    Initial import
    ComboVar class: ttk.Combobox with tk.StringVar

---
+++ combo_var.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from tkinter import ttk
+
+
+class ComboVar(ttk.Combobox):
+    def __init__(self, parent, cnf=None, **kw):
+        super(ComboVar, self).__init__(parent, cnf=None, **kw)
+        if isinstance(cnf, dict):
+            kw.update(cnf)
+        if 'textvariable' in kw:
+            self._var = kw.get('textvariable')
+
+    def configure(self, cnf=None, **kw):
+        if cnf is None and not kw:
+            return super().configure()
+        elif isinstance(cnf, str):
+            if cnf == 'textvariable':
+                return self._var
+            else:
+                return self._get_config(cnf)
+        elif isinstance(cnf, dict):
+            kw.update(cnf)
+        return self._set_config(**kw)
+
+    config = configure
+
+    def _get_config(self, option=None):
+        if option:
+            return super().configure(option)
+        else:
+            return super().configure()
+
+    def _set_config(self, **options):
+        if 'textvariable' in options:
+            self._var = options.get('textvariable')
+        return super().configure(**options)
