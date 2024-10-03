56af1b8ad8ea6a4b8eb01503ebb7a43383366137
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-10-03 09:54:59 +0300

    Remove commented out old code

--- askstring.py
+++ askstring.py
@@ -10,34 +10,11 @@ class QueryString(simpledialog._QueryString):
         self.message = kw.pop('message') if 'message' in kw else None
         super().__init__(*args, **kw)
 
-    # def __init__(self, *args, **kw):
-    #     if "show" in kw:
-    #         self.__show = kw["show"]
-    #         del kw["show"]
-    #     else:
-    #         self.__show = None
-    #     self.message = None
-    #     if 'message' in kw:
-    #         self.message = kw.pop('message')
-    #     simpledialog._QueryDialog.__init__(self, *args, **kw)
-
     def body(self, master):
         entry = super().body(master)
         assert entry is self.entry
         entry.insert(0, self.message)
         return entry
-
-    # def body(self, master):
-    #     entry = simpledialog._QueryDialog.body(self, master)
-    #     print(f'{id(entry) = }, {id(self.entry) = }')
-    #     if self.message:
-    #         self.entry.insert(0, self.message)
-    #     if self.__show is not None:
-    #         entry.configure(show=self.__show)
-    #     return entry
-
-    # def getresult(self):
-    #     return self.entry.get()
 
 
 def askstring(title, prompt, message=None, **kw):
