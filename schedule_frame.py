1c0c85d7921745c37fa09a73eb2ae357aa950700
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-10-26 20:06:08 +0300

    Initial import
    A template, needs refining

---
+++ schedule_frame.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import tkinter as tk
+from tkinter import ttk
+from itertools import groupby
+from operator import itemgetter
+
+data = (
+    {"date": "2024-09-01", "exercise": "Squat", "weight": 100, "reps": 5},
+    {"date": "2024-09-02", "exercise": "Bench Press", "weight": 80, "reps": 8},
+    {"date": "2024-09-03", "exercise": "Deadlift", "weight": 120, "reps": 4},
+    {"date": "2024-09-03", "exercise": "Deadlift", "weight": 110, "reps": 5}
+)
+
+
+class ScheduleFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        tree = ttk.Treeview(self, columns=('exercise', 'weight', 'reps'))
+        tree.grid(column=0, row=0, sticky=tk.NSEW)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        tree.heading("#0", text="Date")
+        tree.heading("exercise", text="exercise")
+        tree.heading("weight", text="Weight (kg)")
+        tree.heading("reps", text="Reps")
+        tree.column("#0", width=100)
+        tree.column("exercise", width=150)
+        tree.column("weight", width=100)
+        tree.column("reps", width=100)
+
+        for date, exercises in groupby(data, key=itemgetter('date')):
+            parent = tree.insert("", "end", text=date)
+            for exer in exercises:
+                tree.insert(
+                    parent, 'end', text='',
+                    values=(itemgetter('exercise', 'weight', 'reps')(exer)))
