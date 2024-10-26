7cda121467a2c2a46760bd59dafdebd64167b477
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-10-26 21:19:14 +0300

    Before 'generalizing' the code

--- schedule_frame.py
+++ schedule_frame.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from itertools import groupby
-from operator import itemgetter
 import tkinter as tk
 from tkinter import ttk
 from sqlalchemy import select
 from sqlalchemy.engine.base import Engine
 import models as md
 import database as db
-
-data = (
-    {"date": "2024-09-01", "exercise": "Squat", "weight": 100, "reps": 5},
-    {"date": "2024-09-02", "exercise": "Bench Press", "weight": 80, "reps": 8},
-    {"date": "2024-09-03", "exercise": "Deadlift", "weight": 120, "reps": 4},
-    {"date": "2024-09-03", "exercise": "Deadlift", "weight": 110, "reps": 5}
-)
+# col_names = ['id', 'when', 'weight', 'reps']
+col_names: list[str] = [col.name for col in md.Workout.__table__.columns]
+# rel_names = ['exercise', 'schedule']
+rel_names: list[str] = [rel.key for rel in md.Workout.__mapper__.relationships]
 
 
 class ScheduleFrame(tk.Frame):
     def __init__(self, parent, engine: Engine):
         super().__init__(parent)
         self.engine = engine
-        tree = ttk.Treeview(self, columns=('exercise', 'weight', 'reps'))
+        tree = ttk.Treeview(
+            self, columns=('exercise', 'when', 'weight', 'reps'))
         tree.grid(column=0, row=0, sticky=tk.NSEW)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        tree.heading("#0", text="Date")
-        tree.heading("exercise", text="exercise")
+        tree.heading('#0', text='schedule')
+        tree.heading('exercise', text="exercise")
+        tree.heading('when', text='when')
         tree.heading("weight", text="Weight (kg)")
         tree.heading("reps", text="Reps")
         tree.column("#0", width=100)
         tree.column("exercise", width=150)
+        tree.column('when', width=100)
         tree.column("weight", width=100)
         tree.column("reps", width=100)
 
@@ -42,14 +40,4 @@ class ScheduleFrame(tk.Frame):
                 for wo in schedule.workouts:
                     tree.insert(
                         sch, 'end', text='',
-                        values=(wo.exercise.name, wo.weight, wo.reps))
-
-
-        #         schedule_names.append(schedule.name)
-        
-        # for date, exercises in groupby(data, key=itemgetter('date')):
-        #     parent = tree.insert("", "end", text=date)
-        #     for exer in exercises:
-        #         tree.insert(
-        #             parent, 'end', text='',
-        #             values=(itemgetter('exercise', 'weight', 'reps')(exer)))
+                        values=(wo.exercise.name, wo.when, wo.weight, wo.reps))
