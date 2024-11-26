aa3099e07370e52993c0cdf25c9e4c671de35b5e
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2024-11-26 20:06:35 +0200

    Refactor refresh_view()
    date_wo.clear() , etc.,

--- retrospect_frame.py
+++ retrospect_frame.py
@@ -36,22 +36,22 @@ class RetrospectView(ScrolledTreeview):
             if values and values[0]:
                 schedule_name = get_schedule_name(values[0])
                 msg = f'Jump to <{schedule_name}>'
-                jump = askokcancel(title=__name__, message=msg, parent=self)
-                if jump:
-                    # Jump to schedule tab, expand the tree below `schedule_name'
-                    pass
+                # jump = askokcancel(title=__name__, message=msg, parent=self)
+                # if jump:
+                #     # Jump to schedule tab, expand the tree below `schedule_name'
+                #     pass
 
     def refresh_view(self):
         self.delete(*self.get_children())
         name_wo: defaultdict[str, list[md.Workout]] = defaultdict(list)
+        date_wo: defaultdict[datetime, list[md.Workout]] = defaultdict(list)
         with db.session_scope(self.engine) as session:
             query = select(md.Workout)
             for wo in session.scalars(query):
                 name_wo[wo.exercise.name].append(wo)
             for exer_name, workouts in name_wo.items():
                 exer_node = self.insert('', 'end', text=exer_name)
-                date_wo: defaultdict[datetime, list[md.Workout]]
-                date_wo = defaultdict(list)
+                date_wo.clear()
                 for wo in workouts:
                     dt = datetime.strptime(wo.date(), '%Y-%m-%d')
                     date_wo[dt].append(wo)
@@ -60,10 +60,9 @@ class RetrospectView(ScrolledTreeview):
                         exer_node, 'end', values=(wo.date(True), ))
                     _workouts = sorted(workouts, key=lambda wo: wo.weight,
                                        reverse=True)
-                    for wo in _workouts:
-                        self.insert(date_node, 'end',
-                                    values=('', wo.weight, wo.reps))
-                        break
+                    w0 = _workouts[0]
+                    self.insert(date_node, 'end',
+                                values=('', w0.weight, w0.reps))
 
 
 class RetrospectFrame(tk.Frame):
